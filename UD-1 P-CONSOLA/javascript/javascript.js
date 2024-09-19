3030822e45454b19383c4faaea35f73561739175
Barroso18 <david.barrosomartinez@gmail.com>
2024-09-19 09:47:49 +0200

    Segundo commit

--- UD-1 P-CONSOLA/javascript/javascript.js
+++ UD-1 P-CONSOLA/javascript/javascript.js
@@ -1,2 +1,23 @@
 console.log("Hola, se esta cargando el Javascript");
 let nombre = "David";
+console.log(nombre);
+console.warn(nombre);
+console.error(nombre);
+
+
+let empleados = [
+    {'nombre':'Marcos', 'edad':24},
+    {'nombre':'Lena', 'edad':23},
+    {'nombre':'Omar', 'edad':27},
+];
+// console.log(empleados);
+console.table(empleados);
+//Comentario una linea
+/* Comentario varias 
+lineas */
+
+//console.log("$cHola Mundo","color:blue");
+//console.log("$cHola Mundo","font-size:20px");
+
+
+
