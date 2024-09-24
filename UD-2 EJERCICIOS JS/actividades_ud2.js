34729fc701686440bcd48b4fdf621f6fe43d8b79
Barroso18 <david.barrosomartinez@gmail.com>
2024-09-24 13:04:17 +0200

    Ejercicios 1 y 2 hechos

--- UD-2 EJERCICIOS JS/actividades_ud2.js
+++ UD-2 EJERCICIOS JS/actividades_ud2.js
@@ -1,7 +1,24 @@
-let nombre;
-let apellido;
-let a;
-let b;
-let c;
-nombre = prompt("Introduce tu nombre:");
-apellido = prompt("Introduce tu apellido:");
+//Ejercicio 1
+/*
+let nombre = prompt("Introduce tu nombre:");
+let apellido = prompt("Introduce tu apellido:");
+
+let valores = prompt("introduce 3 valores numericos, separados por comas");
+let valoresArray = valores.split(",");
+console.log(`Nombre completo ${nombre}`);
+let suma = 0;
+
+for(let i = 0; i<3;i++){
+    suma += parseInt(valoresArray[i])
+}
+console.log(`La suma es: ${suma}`)
+console.log(`La division es ${parseInt(valoresArray[0])/parseInt(valoresArray[2])}`)
+*/
+//Ejercicio 2
+let valores2 = prompt("Introduce 3 valores numericos enteros, separados por comas");
+let valoresArray2 = valores2.split(",");
+for(let i = 0; i<3;i++){
+    if(valoresArray2[i]> 10){
+        console.log(`El ${valoresArray2[i]} es mayor que 10`);
+    }
+}
