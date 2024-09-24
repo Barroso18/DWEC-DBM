d2ec05889ddb8d16b6e634fa7977f799eb34475a
Barroso18 <david.barrosomartinez@gmail.com>
2024-09-24 13:15:05 +0200

    Ejercicio 3 Terminado

--- UD-2 EJERCICIOS JS/actividades_ud2.js
+++ UD-2 EJERCICIOS JS/actividades_ud2.js
@@ -15,6 +15,7 @@ console.log(`La suma es: ${suma}`)
 console.log(`La division es ${parseInt(valoresArray[0])/parseInt(valoresArray[2])}`)
 */
 //Ejercicio 2
+/*
 let valores2 = prompt("Introduce 3 valores numericos enteros, separados por comas");
 let valoresArray2 = valores2.split(",");
 for(let i = 0; i<3;i++){
@@ -22,3 +23,16 @@ for(let i = 0; i<3;i++){
         console.log(`El ${valoresArray2[i]} es mayor que 10`);
     }
 }
+*/
+
+//Ejercicio 3
+/*
+let dia = prompt("Introduce el dia:");
+let mes = prompt("Introduce mes(numero):");
+dia = parseInt(dia);
+mes = parseInt(mes);
+if(dia ===14 && mes == 2){
+    alert("ES SAN VALENTIN");
+}*/
+
+//Ejercicio 4
