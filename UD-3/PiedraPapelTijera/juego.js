fa6002f4c17d0ac8d03031edd70efc284bf1abbb
Barroso18 <david.barrosomartinez@gmail.com>
2024-10-10 09:26:23 +0200

    Terminado juego y funcional

--- UD-3/PiedraPapelTijera/juego.js
+++ UD-3/PiedraPapelTijera/juego.js
@@ -1,4 +1,35 @@
 const PIEDRA = 'piedra'
 const PAPEL = 'papel'
 const TIJERA = 'tijera'
+const CONDICIONGANAR = [`${PIEDRA}-${TIJERA}`,`${TIJERA}-${PAPEL}`,`${PAPEL}-${PIEDRA}`]
 const opciones = [PIEDRA,PAPEL,TIJERA]
+jugar()
+function obtenerEleccionComputadora(){
+    //Math.random()
+    let i = Math.floor(Math.random()*3);
+    return opciones[i];
+}
+
+function determinarGanador(jugador,computadora){
+    if(jugador == computadora){
+        return "Es un empate";
+    }else if(CONDICIONGANAR.find((elemento)=> elemento === `${jugador}-${computadora}`)){// CONDICIONGANAR.find() -> `${jugador}-${computadora}`
+        return "¡Ganaste!";
+    }else{
+        return "La computadora gana";
+    }
+}
+
+function jugar(){
+    let eleccionJugador = -1;
+    let eleccionComputadora = -1;
+    do{
+        eleccionJugador = parseInt(prompt("Selecciona: piedra(0), papel(1), tijera(2)"));
+    }while(eleccionJugador >2 || eleccionJugador <0){
+        eleccionComputadora = obtenerEleccionComputadora();
+        let resultado = determinarGanador(opciones[eleccionJugador],eleccionComputadora);
+        console.log(`Eleccion jugador ${opciones[eleccionJugador]} , eleccion computadora ${eleccionComputadora}`);
+        console.log(resultado);
+
+    }
+}
