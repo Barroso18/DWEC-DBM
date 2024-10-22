b330d0ef877c19737bd2e8e01e41150ede2d780d
Barroso18 <david.barrosomartinez@gmail.com>
2024-10-22 13:01:08 +0200

    eventos

--- UD-4 DOM/Ejercicos UD4/scriptEjercicio3UD4.js
+++ UD-4 DOM/Ejercicos UD4/scriptEjercicio3UD4.js
@@ -1,9 +1,20 @@
 //  Ejercicio 3
 let btnCrear = document.getElementById("idcrear");
 btnCrear.addEventListener("click",insertarCheckbox);
+/*let divBorrar = document.getElementById("idDiv");
+divBorrar.addEventListener("click",function(){
+    this.remove();
+});*/
 function insertarCheckbox(){
+    /*let checkboxAntiguos = document.getElementsByTagName("input");
+    if(checkboxAntiguos.length != 0){
+        checkboxAntiguos.
+    }*/
+        //divBorrar.remove();
     let cantidad = parseInt(document.getElementById("idNumero").value);
     console.log(cantidad)
+    let div = document.createElement("div");
+    div.setAttribute("id","idDiv");
     for(let j = 0;j<cantidad; j++){
         let k = Math.random();
         let input = document.createElement("input");
@@ -13,7 +24,11 @@ function insertarCheckbox(){
         input.setAttribute("value",k);
         label.setAttribute("for",`id${k}`);
         label.innerHTML = k+ "<br/>";
-        document.body.appendChild(input);
-        document.body.appendChild(label);
+        div.appendChild(input);
+        div.appendChild(label);
+        document.body.appendChild(div);
     }
+    /*div.addEventListener("click",function(){
+        this.nextElementSibling.remove();
+    });*/
 }
