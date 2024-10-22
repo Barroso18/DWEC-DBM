e88be8160d107a515c64c1755318e4f518fed9e9
Barroso18 <david.barrosomartinez@gmail.com>
2024-10-22 13:19:11 +0200

    Continuar ejercicio 4

--- UD-4 DOM/Ejercicos UD4/scriptEjercicio4UD4.js
+++ UD-4 DOM/Ejercicos UD4/scriptEjercicio4UD4.js
@@ -1,1 +1,9 @@
 //  Ejercicio 4
+let deporte = document.getElementById("iddeporte");
+let serie = document.getElementById("idserie");
+let pelicula = document.getElementById("idpelicula");
+
+let aceptar = document.getElementById("idaceptar");
+aceptar.addEventListener("click",function (){
+
+});
