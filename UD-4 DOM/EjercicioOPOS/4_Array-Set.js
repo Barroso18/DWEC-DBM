277a8dd805aea71b84ca005b6e70ae68fd951216
Barroso18 <david.barrosomartinez@gmail.com>
2024-11-07 10:14:50 +0100

    feaf

--- UD-4 DOM/EjercicioOPOS/4_Array-Set.js
+++ UD-4 DOM/EjercicioOPOS/4_Array-Set.js
@@ -1,15 +1,68 @@
-const autores = [
-    new Set([{ autor: "Juan Rulfo", titulo: "Pedro Páramo", año: "2014" }]),
-    new Set([{ autor: "Juan Rulfo", titulo: "El Llano en Llamas", año: "2013" }]),
-    new Set([{ autor: "Rosa Montero", titulo: "Cuentos verdaderos", año: "2024" }]),
-    new Set([{ autor: "Pablo Neruda", titulo: "Veinte poemas de amor y una canción desesperada", año: "2013" }]),
-    new Set([{ autor: "Almudena Grandes", titulo: "La madre de Frankenstein", año: "2020" }]),
-    new Set([{ autor: "Miguel de Cervantes Saavedra", titulo: "Don Quijote de la Mancha", año: "1605" }]),
-    new Set([{ autor: "Jorge Luis Borges", titulo: "Ficciones", año: "2014" }]),
-    new Set([{ autor: "Rosa Chacel", titulo: "Memorias de Leticia Valle", año: "1945" }]),
-    new Set([{ autor: "Pablo Neruda", titulo: "Antología poética", año: "2014" }]),
-    new Set([{ autor: "Rosa Chacel", titulo: "Saturnal", año: "1972" }]),
-    new Set([{ autor: "Carmen Laforet", titulo: "La Isla de los Demonios", año: "1952" }]),
-    new Set([{ autor: "Jorge Luis Borges", titulo: "El sur", año: "1953" }]),
-    new Set([{ autor: "Pablo Neruda", titulo: "Confieso que he vivido", año: "2010" }])
+const libros = [
+    new Set([ "Juan Rulfo",  "Pedro Páramo",  "2014" ]),
+    new Set([ "Juan Rulfo",  "El Llano en Llamas",  "2013" ]),
+    new Set([ "Rosa Montero",  "Cuentos verdaderos",  "2024" ]),
+    new Set([ "Pablo Neruda",  "Veinte poemas de amor y una canción desesperada",  "2013" ]),
+    new Set([ "Almudena Grandes",  "La madre de Frankenstein",  "2020" ]),
+    new Set([ "Miguel de Cervantes Saavedra",  "Don Quijote de la Mancha",  "1605" ]),
+    new Set([ "Jorge Luis Borges",  "Ficciones",  "2014" ]),
+    new Set([ "Rosa Chacel",  "Memorias de Leticia Valle",  "1945" ]),
+    new Set([ "Pablo Neruda",  "Antología poética",  "2014" ]),
+    new Set([ "Rosa Chacel",  "Saturnal",  "1972" ]),
+    new Set([ "Carmen Laforet",  "La Isla de los Demonios",  "1952" ]),
+    new Set([ "Jorge Luis Borges",  "El sur",  "1953" ]),
+    new Set([ "Pablo Neruda",  "Confieso que he vivido",  "2010" ])
 ];
+function crearElemento(tipo, contenido, padre) {
+    // Crear el elemento del tipo especificado
+    let hijo = document.createElement(tipo)
+    // Indicamos el contenido
+    hijo.innerHTML = contenido
+    //añadir el nodo al documento
+    padre.appendChild(hijo)
+
+    hijo.addEventListener("click", function () {
+        this.remove()
+    })
+
+    return hijo
+
+
+}
+
+
+let buscaAutor = document.getElementById("idAutores");
+
+
+let autores = new Set();
+libros.forEach(element => {
+    if(!autores.has(element.get("autor"))){
+        autores.add(element.get("autor"));
+    }
+});
+
+console.log(autores);
+
+autores.forEach(element=>{
+    let opcion = document.createElement("option");
+    opcion.value = element;
+    opcion.innerHTML = element;
+    buscaAutor.appendChild(opcion)
+})
+function muestralibros(evento){
+   
+    let cuerpo = document.getElementById("idcuerpoTabla");
+    cuerpo.innerHTML=` `;
+    
+    libros.forEach(element=>{
+        if(element.get("autor") === evento.target.value){
+            let tr = crearElemento("tr",``,cuerpo);
+            //let tdTitulo = 
+            crearElemento("td",`${element.get("titulo")}`,tr);
+            //let tdFecha = 
+            crearElemento("td",`${element.get("año")}`,tr);;
+            
+        }
+    })
+}
+buscaAutor.addEventListener("change",muestralibros);
