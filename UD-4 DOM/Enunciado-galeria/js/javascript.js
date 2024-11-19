2ef4864b306502a0d9340548a90352b1ed3641b5
Barroso18 <david.barrosomartinez@gmail.com>
2024-11-19 13:15:04 +0100

    arreglado y funcional

--- UD-4 DOM/Enunciado-galeria/js/javascript.js
+++ UD-4 DOM/Enunciado-galeria/js/javascript.js
@@ -1,35 +1,35 @@
 let images = {
-    "thumb1": {
+    "img_1": {
         "title": "Lakes close to Ljubljana",
         "description": "Incredible water in Ljubljana, Slovenia.",
         "img": "http://farm3.staticflickr.com/2807/8755627802_727863f09e_b.jpg"
     },
-    "thumb2": {
+    "img_2": {
         "title": "Balloons in Cappadocia",
         "description": "Balloon flight in Cappadocia, Turkey.",
         "img": "http://farm6.staticflickr.com/5458/8899898917_fee4ba1613_b.jpg"
     },
-    "thumb3": {
+    "img_3": {
         "title": "Lucía in the Steppe",
         "description": "My motorcycle in the side of the road at some point between the Russian border and Atyrau, Kazakhstan.",
         "img": "http://farm6.staticflickr.com/5459/9045535798_f3d21b27a1_b.jpg"
     },
-    "thumb4": {
+    "img_4": {
         "title": "Charyn Canyon",
         "description": "Charyn Canyon, Kazakhstan.",
         "img": "http://farm3.staticflickr.com/2847/9281765203_d6af908f9f_b.jpg"
     },
-    "thumb5": {
+    "img_5": {
         "title": "Uran Togoo",
         "description": "Road to Uran Togoo, Mongolia.",
         "img": "http://farm8.staticflickr.com/7301/9476752141_969cc26a71_b.jpg"
     },
-    "thumb6": {
+    "img_6": {
         "title": "Vancouver Graffiti",
         "description": "Graffiti in Vancouver. Canada.",
         "img": "http://farm8.staticflickr.com/7379/10139131563_3ddd73c6d4_b.jpg"
     },
-    "thumb7": {
+    "img_7": {
         "title": "Kinkakuji",
         "description": "Kinkakuji, Kyoto. Japan.",
         "img": "http://farm8.staticflickr.com/7365/10138806215_e25e389de3_b.jpg"
