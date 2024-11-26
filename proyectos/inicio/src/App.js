ce5de4a620fa28ccad769f4330be3d93f693b296
Barroso18 <david.barrosomartinez@gmail.com>
2024-11-26 13:02:13 +0100

    Creando una plantilla de proyecto

--- proyectos/inicio/src/App.js
+++ proyectos/inicio/src/App.js
@@ -1,17 +1,9 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          David BM
-        </p>
-        
-      </header>
-    </div>
+    <div>Hola David Barroso</div>
   );
 }
 
